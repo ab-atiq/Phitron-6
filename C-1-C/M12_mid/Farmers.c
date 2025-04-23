d2889a29c7847b350fd54d9297ac714b5c8a6eb0
AB Atiqur <abulbasharatiqur@gmail.com>
2025-04-23 21:48:04 +0600

    w3c2

--- C-1-C/M12_mid/Farmers.c
+++ C-1-C/M12_mid/Farmers.c
@@ -10,18 +10,22 @@ int main()
         scanf("%lf %lf %lf", &m1, &m2, &d);
         double result = d * m1 / (m1 + m2);
         // printf("%lf %lf\n", d, result);
-        double dif = result - d;
+        double dif = d - result;
         if (dif < 0)
         {
             dif = dif * -1;
         }
-        printf("%lf\n", dif);
+
+        if (dif - (int)dif == 0)
+            printf("%d\n", (int)dif);
+        else
+            printf("%d\n", (int)dif + 1);
     }
 
     return 0;
 }
 
-/*  
+/*
 #include <stdio.h>
 #include <math.h>
 
--- a.c
+++ a.c
@@ -3,8 +3,52 @@ int main()
 {
     int n;
     scanf("%d", &n);
+    if (n >= 0)
+    {
+        int a[n];
+        // input for loop
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+        // for max
+        int max = a[0];
+        int min = a[0];
+        for (int i = 0; i < n; i++)
+        {
 
-    printf("%d ", n);
+            if (max < a[i])
+            {
+                max = a[i];
+            }
+            if (min > a[i])
+            {
+                min = a[i];
+            }
+        }
+        // check max and min
+        // printf("%d %d\n", max, min);
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == max)
+            {
+                a[i] = min;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == min)
+            {
+                a[i] = max;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", a[i]);
+        }
+    }
 
     return 0;
 }
