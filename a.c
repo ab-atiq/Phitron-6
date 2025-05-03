1f7a03453a03f59473bf3e91c74c0a70f6b974f4
AB Atiqur <abulbasharatiqur@gmail.com>
2025-05-03 15:54:12 +0600

    codefest-01 C

--- a.c
+++ a.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 int main()
 {
-    int t;
-    scanf("%d", &t);
-    for (int i = 0; i < t; i++)
+    for (int i = 1; i <= 3; i++)
     {
-        char a[101];
-        scanf("%s", a);
-        // code
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
     }
 
     return 0;
