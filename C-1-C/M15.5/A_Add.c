38e1a612d119a5959476a77cff26dd020ebe389c
AB Atiqur <abulbasharatiqur@gmail.com>
2025-05-07 20:14:54 +0600

    M15 & M15.5

---
+++ C-1-C/M15.5/A_Add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+
+int main()
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d\n", add(x, y));
+    return 0;
+}
+
+/*  
+5 10
+15
+*/
