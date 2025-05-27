c17d97ffdbbc200e50683b4cfe11def14f4b25cf
AB Atiqur <abulbasharatiqur@gmail.com>
2025-05-28 03:41:42 +0600

    M3

---
+++ C-2-CPP/module3/built_in_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+/*
+input:
+5
+54 23 5 2 6
+
+output:
+2 5 6 23 54
+*/
