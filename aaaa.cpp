b3bbd78d3df0d5ded0d5a1446822529606af0049
AB Atiqur <abulbasharatiqur@gmail.com>
2025-06-04 20:11:08 +0600

    M7 & M7.5

--- aaaa.cpp
+++ aaaa.cpp
@@ -1,11 +1,34 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+void print(stringstream &ss) // প্যারামিটারে আসা স্ট্রিংটি রিসিভ করা হচ্ছে স্ট্রিংস্ট্রিম এবং রেফারেন্স দিয়ে।
 
-    int a, b;
-    cin >> a >> b;
-    cout << a + b << "\n";
+{
+
+    string word;
+
+    if (ss >> word) // প্রতিবার একটি করে ওয়ার্ড নেওয়া হচ্ছে স্ট্রিংস্ট্রিম থেকে।
+
+    {
+
+        cout << word << endl; // ওয়ার্ডটি প্রিন্ট করা হচ্ছে।
+
+        print(ss); // রিকারশন ফাংশনকে কল করা হচ্ছে।
+    }
+}
+
+int main()
+
+{
+
+    string s;
+
+    getline(cin, s); // স্ট্রিং ইনপুট নেওয়া হচ্ছে।
+
+    stringstream ss(s); // স্ট্রিংস্ট্রিম এর মধ্যে স্ট্রিংটি দিয়ে দেওয়া হচ্ছে।
+
+    print(ss); // ফাংশন কল করে স্ট্রিংটি দিয়ে দেওয়া হচ্ছে।
+
+    return 0;
 }
