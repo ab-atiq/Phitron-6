fc0026701af90023d0ccd46ccc5028b85763f396
AB Atiqur <abulbasharatiqur@gmail.com>
2025-08-11 12:43:22 +0600

    C-6 CPS

--- D_Triple.cpp
+++ D_Triple.cpp
@@ -3,38 +3,32 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
-
     while (t--)
     {
         int n;
         cin >> n;
-        int fre[n + 1];
-        for (int i = 0; i <= n; i++)
-        {
-            fre[i] = 0;
-        }
+        vector<int> a(n);
         for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        vector<int> freq(n + 1, 0); 
+        int ans = -1;
+
+        for (int x : a)
         {
-            int x;
-            cin >> x;
-            fre[x]++;
-        }
-        bool is_three = false;
-        for (int i = 1; i <= n; i++)
-        {
-            if (fre[i] >= 3)
+            freq[x]++;
+            if (freq[x] == 3 && ans == -1)
             {
-                is_three = true;
-                cout << i << endl;
-                break;
+                ans = x;
             }
         }
-        if (!is_three)
-        {
-            cout << -1 << endl;
-        }
+
+        cout << ans << "\n";
     }
     return 0;
 }
