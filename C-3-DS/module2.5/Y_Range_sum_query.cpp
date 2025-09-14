b0d44f19d79458c299a9c86cc577f19d418a500f
AB Atiqur <abulbasharatiqur@gmail.com>
2025-09-14 21:43:34 +0600

    DS overview 123

--- C-3-DS/module2.5/Y_Range_sum_query.cpp
+++ C-3-DS/module2.5/Y_Range_sum_query.cpp
@@ -1,3 +1,5 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Y
+
 // Next module provide optimization of this code
 #include <iostream>
 #include <vector>
--- aa.cpp
+++ aa.cpp
@@ -1,20 +1,33 @@
-// cycle detect undirected graph
-
 #include <bits/stdc++.h>
 using namespace std;
-void fu()
-{
-    int node, edge;
-    // cin >> node >> edge;
-    cout << node << edge;
-}
+
 int main()
 {
-    fu();
+
+    list<int> li;
+    while (1)
+    {
+
+        int x;
+        cin >> x;
+        if (x == -1)
+            break;
+
+        li.push_back(x);
+    }
+
+    int mx = INT_MIN;
+    int mn = INT_MAX;
+
+    // int sz = li.size();
+
+    for (auto it = li.begin(); it != li.end(); ++it)
+    {
+
+        cout << *it << " ";
+    }
+
+    cout << mx - mn << endl;
+
     return 0;
 }
-
-/*
-
-
-*/
