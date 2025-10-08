ffeb641da4bc77f5c911069af9c242999e995167
AB Atiqur <abulbasharatiqur@gmail.com>
2025-10-08 21:54:39 +0600

    C W1C2

--- a.c
+++ a.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
+
 int main()
 {
     int n;
     scanf("%d", &n);
-
-    for (int i = 1; i <= n; i++)
+    for (int i = 8; i <= n; i += 8)
     {
-        if (i % 2 == 0)
-        {
-            printf("%d\n", i);
-        }
+        printf("%d\n", i);
     }
 
     return 0;
