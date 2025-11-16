caa58394bdc4e639e9c09997195506416bb6e898
ab-atiq <abulbasharatiqur@gmail.com>
2025-11-16 19:23:35 +0600

    modify

--- C-1-C/M18.5/F_Front_End.c
+++ C-1-C/M18.5/F_Front_End.c
@@ -55,3 +55,34 @@ int main()
 output:
 1 5 2 4 3
 */
+
+// solution using recursion
+#include <stdio.h>
+void rec(long long int a[], int n, int i)
+{
+    if (i > n - 1 - i)
+    {
+        return;
+    }
+    printf("%lld ", a[i]);
+    if (i != n - 1 - i)
+    {
+        printf("%lld ", a[n - 1 - i]);
+    }
+    rec(a, n, i + 1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    long long int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%lld", &a[i]);
+    }
+
+    rec(a, n, 0);
+
+    return 0;
+}
--- C-1-C/M19.5/J_Lucky_Division.c
+++ C-1-C/M19.5/J_Lucky_Division.c
@@ -26,12 +26,14 @@ int main()
     //     return 0;
     // }
     // Check all numbers from 1 to n to see if any lucky number divides n then that number is almost lucky
-    // 1 - 1000 all lucky numbers are 4, 7, 44, 47, 74, 77, 444, 447, 474, 744, 777
+    // 1 - 1000 all lucky numbers are 4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777. Check Divisible by any one of them.
     for (int i = 1; i <= n; i++)
     {
-        if (isLucky(i))
+        int chk = isLucky(i);
+        if (chk == 1)
+        // if (isLucky(i)) // another way to write
         {
-            if (n % i == 0)
+            if (n % i == 0) // almost lucky number
             {
                 printf("YES\n");
                 return 0;
@@ -61,7 +63,7 @@ int main()
 {
     int x;
     scanf("%d", &x);
-    // 1 - 1000 all lucky numbers are 4, 7, 44, 47, 74, 77, 444, 447, 474, 744, 777. Check Divisible by any one of them.
+    // 1 - 1000 all lucky numbers are 4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777. Check Divisible by any one of them.
     if ((x % 4 == 0) || (x % 7 == 0) || (x % 44 == 0) || (x % 47 == 0) || (x % 74 == 0) || (x % 77 == 0) || (x % 444 == 0) || (x % 447 == 0) || (x % 474 == 0) || (x % 477 == 0) || (x % 744 == 0) || (x % 747 == 0) || (x % 774 == 0) || (x % 777 == 0))
     {
         printf("YES\n");
@@ -70,5 +72,35 @@ int main()
     {
         printf("NO\n");
     }
+    return 0;
+}
+
+// solution using array
+#include <stdio.h>
+int main()
+{
+    int lucky_numbers[] = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+    int n;
+    scanf("%d", &n);
+    int is_almost_lucky = 0;
+
+    for (int i = 0; i < sizeof(lucky_numbers) / sizeof(lucky_numbers[0]); i++)
+    {
+        if (n % lucky_numbers[i] == 0)
+        {
+            is_almost_lucky = 1;
+            break;
+        }
+    }
+
+    if (is_almost_lucky)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+
     return 0;
 }
