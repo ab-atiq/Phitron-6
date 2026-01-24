5a9bb0f922297b74181d251d805ccefa68f6b76b
ab-atiq <abulbasharatiqur@gmail.com>
2026-01-24 23:58:15 +0600

    MentorSession

--- ContestByPhitron/semi_final_DS_grand_contest/Mirror_s_Pattern.cpp
+++ ContestByPhitron/semi_final_DS_grand_contest/Mirror_s_Pattern.cpp
@@ -170,3 +170,105 @@ int main()
 
     return 0;
 }
+
+
+
+// /*
+// ________________________
+// |                      |
+// |     AB Atiq          |
+// |______________________|
+// */
+
+// // Problem link:
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     if (n == 1)
+//     {
+//         cout << "1" << endl;
+//     }
+//     else if (n == 2)
+//     {
+//         cout << "12\n21" << endl;
+//     }
+//     else if (n == 3)
+//     {
+//         cout << "123\n2 2\n321" << endl;
+//     }
+//     else if (n == 4)
+//     {
+//         cout << "1234\n2  3\n3  2\n4321" << endl;
+//     }
+//     else if (n == 5)
+//     {
+//         cout << "12345\n2   4\n3   3\n4   2\n54321" << endl;
+//     }
+//     else if (n == 6)
+//     {
+//         cout << "123456\n2    5\n3    4\n4    3\n5    2\n654321" << endl;
+//     }
+//     else if (n == 7)
+//     {
+//         cout << "1234567\n2     6\n3     5\n4     4\n5     3\n6     2\n7654321" << endl;
+//     }
+//     else if (n == 8)
+//     {
+//         cout << "12345678\n2      7\n3      6\n4      5\n5      4\n6      3\n7      2\n87654321" << endl;
+//     }
+//     else if (n == 9)
+//     {
+//         cout << "123456789\n2       8\n3       7\n4       6\n5       5\n6       4\n7       3\n8       2\n987654321" << endl;
+//     }
+//     return 0;
+// }
+
+// loop solutionj
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    // if (n == 1)
+    // {
+    //     cout << "1" << endl;
+    //     return 0;
+    // }
+    
+    // first row print 1 to n
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i;
+    }
+    cout << endl;
+
+    // 2 row to n-1 row print
+    for (int r = 2; r <= n - 1; r++)
+    {
+        // first column value
+        cout << r;
+        // space print
+        for (int s = 1; s <= n - 2; s++)
+        {
+            cout << " ";
+        }
+        // last column value
+        cout << n - r + 1 << endl;
+    }
+
+    // last row print n to 1
+    if (n != 1)
+    {
+        for (int i = n; i >= 1; i--)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
--- ContestByPhitron/semi_final_DS_grand_contest/Silent_Days_in_the_City.cpp
+++ ContestByPhitron/semi_final_DS_grand_contest/Silent_Days_in_the_City.cpp
@@ -96,3 +96,103 @@ int main()
     }
     return 0;
 }
+
+// /*
+// ________________________
+// |                      |
+// |     AB Atiq          |
+// |______________________|
+// */
+
+// // Problem link:
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n + 1];
+//     for (int i = 1; i <= n; i++) // 10^5
+//     {
+//         cin >> arr[i];
+//     }
+
+//     // 10^10
+//     int q;
+//     cin >> q;
+//     while (q--) // 10^5
+//     {
+//         int l, r;
+//         cin >> l >> r;
+//         int cnt = 0;
+//         if (l == 1)
+//         {
+//             // l++;
+//             // l = l + 1;
+//             l = 2;
+//         }
+//         if (r == n)
+//         {
+//             // r--;
+//             r = n - 1;
+//         }
+//         for (int i = l; i <= r; i++) // 10^5
+//         {
+//             if (arr[i] < arr[i - 1] && arr[i] < arr[i + 1])
+//             {
+//                 cnt++;
+//             }
+//         }
+//         cout << cnt << endl;
+//     }
+//     return 0;
+// }
+
+//
+// int prefixCount[100005];
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n + 1];
+    for (int i = 1; i <= n; i++) // 10^5
+    {
+        cin >> arr[i];
+    }
+
+    // int prefixCount[n + 1];
+    vector<int> prefixCount(n + 1, 0);
+    // prefixCount[1] = 0;
+    for (int i = 2; i <= n - 1; i++)
+    {
+        int flag = 0;
+        if (arr[i] < arr[i - 1] && arr[i] < arr[i + 1])
+        {
+            flag = 1;
+        }
+
+        prefixCount[i] = prefixCount[i - 1] + flag;
+    }
+    prefixCount[n] = prefixCount[n - 1];
+
+    int q;
+    cin >> q;
+    while (q--) // 10^5
+    {
+        int l, r;
+        cin >> l >> r;
+
+        // if (l == 1)
+        // {
+        //     // l++;
+        //     // l = l + 1;
+        //     l = 2;
+        // }
+        // TC= O(1)
+        cout << prefixCount[r] - prefixCount[l - 1] << endl;
+    }
+    return 0;
+}
