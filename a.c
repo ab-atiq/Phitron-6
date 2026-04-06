6e261d0d08b3d020d5356491ffc81eb09ecfebfb
ab-atiq <abulbasharatiqur@gmail.com>
2026-04-06 16:19:03 +0600

    sdt week 1

--- a.c
+++ a.c
@@ -4,10 +4,22 @@ ________________________
 |     AB Atiq          |
 |______________________|
 */
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     printf("Hello world");
+//     return 0;
+// }
+
 #include <stdio.h>
 int main()
 {
-    int n;
-    printf("Hello world");
+    int num1;
+    int num2;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    printf("your secoend number is: %d", num2);
+    printf("\nyour first number is: %d", num1);
     return 0;
 }
