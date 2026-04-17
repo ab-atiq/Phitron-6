dc5759e5b47dcf9a4d93c0b34d836d881e068a00
ab-atiq <abulbasharatiqur@gmail.com>
2026-04-17 20:04:11 +0600

    SDT

--- C-1-C/M6/S_Sum_of_Consecutive_Odd_Numbers.c
+++ C-1-C/M6/S_Sum_of_Consecutive_Odd_Numbers.c
@@ -23,3 +23,41 @@ int main()
 
     return 0;
 }
+
+// another solution
+#include <stdio.h>
+int main()
+{
+    int tc;
+    scanf("%d", &tc);
+    while (tc--)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        if (x > y)
+        {
+            int sum = 0;
+            for (int i = y + 1; i < x; i++)
+            {
+                if (i % 2 == 1)
+                    sum += i;
+            }
+            printf("%d\n", sum);
+        }
+        else if (y > x)
+        {
+            int sum = 0;
+            for (int i = x + 1; i < y; i++)
+            {
+                if (i % 2 == 1)
+                    sum += i;
+            }
+            printf("%d\n", sum);
+        }
+        else
+        {
+            printf("0\n");
+        }
+    }
+    return 0;
+}
